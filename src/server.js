d1c25d2deca2fec82fe905df42a708cdeab1f319
Pedro Dominici <pedro.dominici@acct.global>
2021-04-04 14:00:04 -0300

    server adjsuments

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const port = 3000
+const port = 3000
 const cors = require('cors')
 
 const Piadas = require('./data/piadas')
@@ -9,4 +9,8 @@ app.use(cors())
 app.get('/', (req, res) => {
     let number = Math.floor(Math.random() * Piadas.length);
     res.json(Piadas[number])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
